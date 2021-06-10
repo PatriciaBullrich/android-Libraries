7c56f98eca16eb1af40a701392464eef1420cb58
PatriciaBullrich <82680098+PatriciaBullrich@users.noreply.github.com>
2021-06-10 13:35:16 -0300

    Update LogManager.java

--- utiles/LogManager.java
+++ utiles/LogManager.java
@@ -2,7 +2,7 @@
 import android.util.Log;
 
 public final class LogManager {
-    public static final String CUSTOM_TAG ="Custom Tag"
+    public static final String CUSTOM_TAG ="Custom Tag";
     public static void log(String msj){
         Log.d(CUSTOM_TAG, msj);
     }
