fef12400a79e14ea788fc6c7e6c95a1be2ca588a
PatriciaBullrich <82680098+PatriciaBullrich@users.noreply.github.com>
2021-07-13 09:10:02 -0300

    Update AsyncPostBase.java

--- AsyncPostBase.java
+++ AsyncPostBase.java
@@ -11,37 +11,89 @@ import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
 //THIS CLASS IS INTENDED TO BE INHERITED
-public class AsyncPostBase extends AsyncTask<String,Void,String> {
-   protected JSONObject jsonParam = new JSONObject(); //in the child class you fill this value with the body params
-    @Override
-    protected String doInBackground(String... params) {
-        String response ="";
-        try{
-            URL url = new URL(params[0]);
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-            connection.setDoOutput(true);
-            connection.setRequestMethod("POST");
-            connection.setRequestProperty("Content-Type", "application/json");
-            connection.setRequestProperty("Accept", "application/json");
-             if(!Session.currentUser.ApiKey.equals("")) connection.setRequestProperty("ApiKey",Session.currentUser.ApiKey); // optional api key
-            OutputStreamWriter outputStream = new OutputStreamWriter(connection.getOutputStream());
-            outputStream.write(jsonParam.toString());
-            outputStream.flush();
-            outputStream.close();
+    public class AsyncPostBase extends AsyncTask<Void,Void,String> {
+        protected JSONObject jsonParam = new JSONObject(); //in the child class you fill this value with the body params
+        protected String url;
+        public AsyncPostBase(String url){
+            this.url= url;
+        }
 
-            InputStream cuerpoRespuesta = connection.getInputStream();
-            InputStreamReader lector = new InputStreamReader(cuerpoRespuesta,"UTF-8");
-            BufferedReader r = new BufferedReader(lector);
-            StringBuilder total = new StringBuilder();
-            for (String line; (line = r.readLine()) != null; ) {
-                total.append(line);
+        public void setParams(String key,String value) {
+            try {
+                jsonParam.put(key, value);
+            } catch (Exception e) {
+                //CustomLog.logException(e);
             }
-            response = total.toString();
-            CustomLog.log(connection.getResponseMessage());
         }
-        catch (Exception ex){
-            CustomLog.logException(e);
+
+        public void setParams(String key,int value) {
+            try {
+                jsonParam.put(key, value);
+            } catch (Exception e) {
+                //CustomLog.logException(e);
+            }
         }
-        return response;
+
+        public void setParams(String key, Date value) {
+            try {
+                jsonParam.put(key, value);
+            } catch (Exception e) {
+                //CustomLog.logException(e);
+            }
+        }
+
+        public void setParams(String key,float value) {
+            try {
+                jsonParam.put(key, value);
+            } catch (Exception e) {
+                //CustomLog.logException(e);
+            }
+        }
+
+        public void setParams(String key,double value) {
+            try {
+                jsonParam.put(key, value);
+            } catch (Exception e) {
+                //CustomLog.logException(e);
+            }
+        }
+
+        public void setParams(String key,boolean value) {
+            try {
+                jsonParam.put(key, value);
+            } catch (Exception e) {
+                //CustomLog.logException(e);
+            }
+        }
+        @Override
+        protected String doInBackground(Void... params) {
+            String response ="";
+            try{
+                URL url = new URL(url);
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.setDoOutput(true);
+                connection.setRequestMethod("POST");
+                connection.setRequestProperty("Content-Type", "application/json");
+                connection.setRequestProperty("Accept", "application/json");
+               // if(!Session.currentUser.ApiKey.equals("")) connection.setRequestProperty("ApiKey",Session.currentUser.ApiKey); // optional api key
+                OutputStreamWriter outputStream = new OutputStreamWriter(connection.getOutputStream());
+                outputStream.write(jsonParam.toString());
+                outputStream.flush();
+                outputStream.close();
+
+                InputStream cuerpoRespuesta = connection.getInputStream();
+                InputStreamReader lector = new InputStreamReader(cuerpoRespuesta,"UTF-8");
+                BufferedReader r = new BufferedReader(lector);
+                StringBuilder total = new StringBuilder();
+                for (String line; (line = r.readLine()) != null; ) {
+                    total.append(line);
+                }
+                response = total.toString();
+                //CustomLog.log(connection.getResponseMessage());
+            }
+            catch (Exception ex){
+                //CustomLog.logException(e);
+            }
+            return response;
+        }
     }
-}
