0f7873e7b94867cd9124f5b1ca0bd777fe493f67
PatriciaBullrich <82680098+PatriciaBullrich@users.noreply.github.com>
2021-07-26 20:43:01 -0300

    add the possibility to send params through request body

--- AsyncTaskBase.java
+++ AsyncTaskBase.java
@@ -1,16 +1,96 @@
 //Generic Async task to extend for GET requests
 
-import com.example.tpombd.utiles.StreamHelper;
+package com.example.ProyectoFinal.loangrounds.AsyncTask;
+
 import android.os.AsyncTask;
+
+import com.example.ProyectoFinal.loangrounds.Utilidades.CustomLog;
+import com.example.ProyectoFinal.loangrounds.Utilidades.OutputStreamHelper;
+import com.example.ProyectoFinal.loangrounds.Utilidades.StreamHelper;
+
+import org.json.JSONObject;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Date;
+
 public class AsyncTaskBase extends AsyncTask<Void, Void ,String> {
 
-    private String Busqueda;
+    private String url;
+    protected JSONObject jsonParam = new JSONObject(); //in the child class you fill this value with the body params
 
-    public AsyncTaskBase(String b) {
-        this.Busqueda = b;
+    public AsyncTaskBase(String url) {
+        this.url = url;
     }
+
+    public void setParams(String key, String value) {
+        try {
+            jsonParam.put(key, value);
+        } catch (Exception e) {
+            //CustomLog.logException(e);
+        }
+    }
+
+    public void setParams(String key, int value) {
+        try {
+            jsonParam.put(key, value);
+        } catch (Exception e) {
+            //CustomLog.logException(e);
+        }
+    }
+
+    public void setParams(String key, Date value) {
+        try {
+            jsonParam.put(key, value);
+        } catch (Exception e) {
+            //CustomLog.logException(e);
+        }
+    }
+
+    public void setParams(String key, float value) {
+        try {
+            jsonParam.put(key, value);
+        } catch (Exception e) {
+            //CustomLog.logException(e);
+        }
+    }
+
+    public void setParams(String key, double value) {
+        try {
+            jsonParam.put(key, value);
+        } catch (Exception e) {
+            //CustomLog.logException(e);
+        }
+    }
+
+    public void setParams(String key, boolean value) {
+        try {
+            jsonParam.put(key, value);
+        } catch (Exception e) {
+            //CustomLog.logException(e);
+        }
+    }
+
     @Override
     protected String doInBackground(Void... voids) {
-        return StreamHelper.returnJsonAsString(Busqueda);
+        String response = "";
+        try {
+            URL request = new URL(url);
+            CustomLog.logObject(request);
+            HttpURLConnection con = (HttpURLConnection) request.openConnection();
+            con.setRequestProperty("ApiKey", "jorge"/*Session.currentUser.getApiKey()*/);
+            CustomLog.log("connecting");
+            if (con.getResponseCode() == 200) {
+                CustomLog.log("Connection OK");
+                if (jsonParam.length() >0) OutputStreamHelper.writeOutPut(con.getOutputStream(),jsonParam);
+                response = StreamHelper.returnJsonAsString(con.getInputStream());
+            } else {
+                CustomLog.log("error when connecting to the api");
+            }
+            con.disconnect();
+        } catch (Exception ex) {
+            CustomLog.logException(ex);
+        }
+        return response;
     }
 }
