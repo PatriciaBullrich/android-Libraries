541015c0ed0e062acfc6b484901ab8b0cc85251a
PatriciaBullrich <82680098+PatriciaBullrich@users.noreply.github.com>
2021-08-04 22:23:11 -0300

    Add files via upload

---
+++ utiles/AlertHelper.java
@@ -0,0 +1,43 @@
+package com.example.tp7_polshu.utiles;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+
+public class AlertHelper {
+
+
+    public static void mostrarAlertaError(Context c, String error, DialogInterface.OnClickListener click){
+        AlertDialog.Builder builder = new AlertDialog.Builder(c);
+        String titulo = "Ups, parece que hubo un error"; // generic text
+         builder.setTitle(titulo);
+        builder.setMessage(error);
+        builder.setPositiveButton("Continuar",click); // you can add listener if you want
+        AlertDialog dialog = builder.create();
+        dialog.setCanceledOnTouchOutside(true);
+        dialog.show();
+    }
+
+    public static void mostrarAlertaError(Context c, String error){
+        mostrarAlertaError(c,error,null);
+    }
+
+    public static void mostrarMensaje(Context c, String mensaje, DialogInterface.OnClickListener click){
+        AlertDialog.Builder builder = new AlertDialog.Builder(c);
+        builder.setMessage(mensaje);
+        builder.setPositiveButton("Continuar",click); // you can add listener if you want
+        builder.setNegativeButton("Cancelar",null);
+        AlertDialog dialog = builder.create();
+        dialog.setCanceledOnTouchOutside(false);
+        dialog.show();
+    }
+
+    public static void mostrarMensaje(Context c, String mensaje){
+       mostrarMensaje(c,mensaje,null);
+    }
+
+
+
+
+
+}
---
+++ utiles/FlashlightHelper.java
@@ -0,0 +1,63 @@
+package com.example.tp7_polshu;
+
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.hardware.camera2.CameraManager;
+import android.os.Build;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+
+import com.example.tp7_polshu.utiles.AlertHelper;
+import com.example.tp7_polshu.utiles.CustomLog;
+
+public class FlashlightHelper {
+
+    /*   <uses-permission android:name="android.permission.CAMERA"/>
+    <uses-permission android:name="android.permission.FLASHLIGHT"/>
+    Add in manifest
+     */
+    private final BaseActivity base = new BaseActivity();
+    private static  CameraManager manager;
+    private static String cameraId;
+    public static boolean estaEncendida = false;
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    private static void inicializarFlashlight(Context context){
+        boolean estaDisponible = context.getPackageManager().
+                hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH);
+        if(!estaDisponible) CustomLog.log("el flash no esta disponible"); //show an error
+        else{
+            manager = (CameraManager) context.getSystemService(Context.CAMERA_SERVICE);
+            try {
+                cameraId = manager.getCameraIdList()[0];
+                manager.registerTorchCallback(statusCallBakck,null);
+            } catch (Exception e){
+                AlertHelper.mostrarAlertaError(context.getApplicationContext(), "parece que hubo un error con tu camara");
+                CustomLog.logException(e);
+            }
+        }
+
+    }
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    private static final CameraManager.TorchCallback statusCallBakck = new CameraManager.TorchCallback() {
+        @Override
+        public void onTorchModeChanged(@NonNull String cameraId, boolean enabled) {
+            super.onTorchModeChanged(cameraId, enabled);
+            estaEncendida = enabled;
+
+        }
+    };
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    public static void switchFlashLight(Context c,boolean btnStatus) {
+        try {
+            if(manager == null) inicializarFlashlight(c);
+            manager.setTorchMode(cameraId, btnStatus);
+        } catch (Exception e) {
+            CustomLog.logException(e);
+        }
+    }
+
+
+}
