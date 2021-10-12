1621017a3d97989e730e50550ab9f3f6cee05359
PatriciaBullrich <82680098+PatriciaBullrich@users.noreply.github.com>
2021-10-12 09:16:59 -0300

    Update taskListener.java

--- AsyncTask/taskListener.java
+++ AsyncTask/taskListener.java
@@ -1,7 +1,7 @@
 
 public interface TaskListener {
     public void onTaskStarted();
-    public void onTaskFinished(String s);
+    public void onTaskFinished(String taskName, String s);
     public void onBadRequest();
     public void onError(Exception ex);
 
